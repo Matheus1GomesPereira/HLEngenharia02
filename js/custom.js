fe12931a669725d345a830e6573d8ff23f457508
Matheus Gomes Pereira <89620294+Matheus1GomesPereira@users.noreply.github.com>
2025-12-26 04:02:40 -0300

    feature: customização do header e cores

--- js/custom.js
+++ js/custom.js
@@ -35,7 +35,7 @@ $(".owl-carousel").owlCarousel({
 /** google_map js **/
 function myMap() {
     var mapProp = {
-        center: new google.maps.LatLng(40.712775, -74.005973),
+        center: new google.maps.LatLng(-15.836996, -48.128004),
         zoom: 18,
     };
     var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
